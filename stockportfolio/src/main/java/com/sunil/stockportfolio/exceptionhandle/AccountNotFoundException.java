1d41bc261c05e288a1f4d15ab07e258224a1c2b7
sunil <sunil.2701k@gmail.com>
2026-06-26 18:18:38 +0530

    feat: implement core entities, authentication services, and stock transaction management logic

---
+++ stockportfolio/src/main/java/com/sunil/stockportfolio/exceptionhandle/AccountNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sunil.stockportfolio.exceptionhandle;
+
+public class AccountNotFoundException extends RuntimeException {
+  public AccountNotFoundException(String message) {
+    super(message);
+  }
+}
---
+++ stockportfolio/src/main/java/com/sunil/stockportfolio/serviceImpl/AccountServiceImpl.java
@@ -0,0 +1,4 @@
+package com.sunil.stockportfolio.serviceImpl;
+
+public class AccountServiceImpl {
+}
